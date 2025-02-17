0ed392e546a69f30a492f48a4e61f2dbaa66ca89
Ftupa <ftup149@aucklanduni.ac.nz>
2025-02-17 22:28:28 +1300

    Add files via upload

---
+++ workshop 2 set internal.js
@@ -0,0 +1,13 @@
+
+
+function setup() {
+  createCanvas(400, 400);
+  background(220);
+  setInterval(redsquare,1000);
+}
+function redsquare (){
+noStroke();
+fill(250,0,0);
+square(random(0,200),random(0,250),80,)
+}
+
